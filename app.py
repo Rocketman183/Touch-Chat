5aae0d89b353b8bca8939238b81cd80401405afb
Dimoffmart <dimoffmart@gmail.com>
2021-04-28 15:30:54 +0500

    Вроде как закончил v1.0

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ db = client.Messages
 collection = db.Messages
 # Настройка окна
 win = Tk()
-win.title('Мессенджер')
+win.title('Touch chat')
 win.geometry('312x348')
 win['bg'] = '#E8FFDA'
 win.resizable(width=False, height=False)
@@ -51,8 +51,8 @@ message_area.place(relwidth=1, relheight = 1)
 
 # Начальный вывод сообщений
 for message in collection.find({}):
-		messages.append(Label(message_area, bg = '#E8FFDA', text = (message['name'] + ' ' + message['time'])).pack())
-		messages.append(Label(message_area, bg = '#E8FFDA', text = message['text']).pack())
+		messages.append(Label(message_area, bg = '#E8FFDA', text = (message['name'] + ' ' + message['time']), font = ('Arial',10, 'bold')).pack())
+		messages.append(Label(message_area, bg = '#E8FFDA', text = message['text'], font = ('Arial', 10)).pack())
 
 # Функция, считающая длину массива с документами и её вызов
 def count_docs():
@@ -65,23 +65,40 @@ def count_docs():
 def upd_messages():
 	message = list(collection.find().sort('time',-1).limit(1))
 	if len(messages) < count_docs()*2:
-		messages.append(Label(message_area, bg  = '#E8FFDA',text = (name + ' ' + message[0]['time'])).pack())
-		messages.append(Label(message_area, bg  = '#E8FFDA',text = message[0]['text']).pack())
+		messages.append(Label(message_area, bg  = '#E8FFDA',text = (name + ' ' + message[0]['time']), font = ('Arial',10, 'bold')).pack())
+		messages.append(Label(message_area, bg  = '#E8FFDA',text = message[0]['text'], font = ('Arial',10)).pack())
 
 	win.after(5000, upd_messages)
 upd_messages()
 
 # Функция, вставляющая сообщение в базу данных и обратно из базы данных в Label
 def send_message():
-	time = datetime.now().strftime('%Y-%m-%d %H:%M:%S') # Время отправки сообщения
-	collection.insert_one({
-		'name': name,
-		'time': time,
-		'text':e_input.get()
-		})
-	last_message = collection.find_one({'text':e_input.get()})
-	messages.append(Label(message_area, bg  = '#E8FFDA',text = (name + ' ' + last_message['time'])).pack())
-	messages.append(Label(message_area, bg  = '#E8FFDA',text = last_message['text']).pack())
+	if e_input.get() != '':
+		time = datetime.now().strftime('%Y-%m-%d %H:%M:%S') # Время отправки сообщения
+		collection.insert_one({ # Отправка в БД
+			'name': name,
+			'time': time,
+			'text':e_input.get()
+			})
+		last_message = collection.find_one({'text':e_input.get()}) # Вставка из БД в окно сообщений
+		messages.append(Label(message_area, bg  = '#E8FFDA',text = (name + ' ' + last_message['time']), font = ('Arial',10, 'bold')).pack())
+		messages.append(Label(message_area, bg  = '#E8FFDA',text = last_message['text'], font = ('Arial',10)).pack())
+		e_input.delete(0, END) # Очистка поля ввода после отправки
+		e_input.insert(0, "")
+
+def send_message_enter(self):
+	if e_input.get() != '':
+		time = datetime.now().strftime('%Y-%m-%d %H:%M:%S') # Время отправки сообщения
+		collection.insert_one({ # Отправка в БД
+			'name': name,
+			'time': time,
+			'text':e_input.get()
+			})
+		last_message = collection.find_one({'text':e_input.get()}) # Вставка из БД в окно сообщений
+		messages.append(Label(message_area, bg  = '#E8FFDA',text = (name + ' ' + last_message['time'])).pack())
+		messages.append(Label(message_area, bg  = '#E8FFDA',text = last_message['text']).pack())
+		e_input.delete(0, END) # Очистка поля ввода после отправки
+		e_input.insert(0, "")
 
 # Функция, регистрирующая имя пользователя и закрывающая приветственное окно
 def name_reg():
@@ -107,7 +124,7 @@ e_input.place(relx = 0.02,
 # Кнопка
 b_send = Button(input_send,
 				text = 'Отправить',
-				bg = 'white',
+				bg = 'green',
 				font = ('Roboto',10),
 				border = 1,
 				command = send_message)
@@ -117,22 +134,23 @@ b_send.place(relx = 0.75,
             relheight = 0.8, 
             relwidth = 0.22)
 
+win.bind('<Return>', send_message_enter)
+
 # Поле ввода имени
-start_win = Frame(win) #Фрейм, на котором будет вводиться имя
+start_win = Frame(win, bg = '#292825') #Фрейм, на котором будет вводиться имя
 start_win.place(relheight = 1, 
             relwidth = 1)
-l_enter = Label(start_win, text = 'Добро пожаловать\nВведите имя') # Приветственная надпись
-l_enter.place(rely = 0.3, relx = 0.3)
+l_enter = Label(start_win, text = 'Добро пожаловать!\nВведите имя:', font = ('Arial',15,'bold'), bg = '#292825', fg = '#FFA300') # Приветственная надпись
+l_enter.place(rely = 0.3, relx = 0.17)
 e_enter = Entry(start_win) # Ввод
 e_enter.place(relx = 0.1,
             rely = 0.5,
 			relheight = 0.07, 
             relwidth = 0.5)
-b_enter = Button(start_win, text = 'Войти', command = name_reg) # Кнопка входа
+b_enter = Button(start_win, text = 'Войти', command = name_reg, bg = '#292825', fg = '#FFA300', activebackground = '#292825', activeforeground = '#FFA300') # Кнопка входа
 b_enter.place(relx = 0.7,
             rely = 0.5,
 			relheight = 0.07, 
             relwidth = 0.2)
-
 
 win.mainloop()
